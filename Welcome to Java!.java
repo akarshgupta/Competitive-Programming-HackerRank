4f6adb0f53eeab93869fbf601966980d58791aa4
Akarsh Gupta <akarshgupta0505@gmail.com>
2018-09-10 15:41:59 +0530

    Create Welcome to Java!.java

---
+++ Welcome to Java!.java
@@ -0,0 +1,11 @@
+# https://www.hackerrank.com/challenges/welcome-to-java/submissions/code/64652807
+# Welcome to Java!
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+        /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
