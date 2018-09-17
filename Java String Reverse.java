8df33e921d30e93049a98347ce92506ac27c12fb
Akarsh Gupta <akarshgupta0505@gmail.com>
2018-09-17 14:28:24 +0530

    Create Java String Reverse.java

---
+++ Java String Reverse.java
@@ -0,0 +1,30 @@
+# Java String Reverse
+# https://www.hackerrank.com/challenges/java-string-reverse/problem
+
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String b =A;
+        String rev = "";
+        for(int i = A.length()-1;i>=0;i--)
+        {
+            rev = rev + A.charAt(i);
+        }
+        if(rev.equals(b))
+            System.out.println("Yes");
+        else
+            System.out.println("No");
+        /* Enter your code here. Print output to STDOUT. */
+        
+    }
+}
+
+
+
